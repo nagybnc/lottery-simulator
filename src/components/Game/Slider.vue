e842bf75bd8bf658871889b886f6566a9e9d2a3f
nagybnc <bencet630@gmail.com>
2023-03-13 20:15:41 +0100

    Fix Slider styles

--- src/components/Game/Slider.vue
+++ src/components/Game/Slider.vue
@@ -1,11 +1,11 @@
 <template>
-    <div className="mb-8 text-xs text-dark-blue sm:text-base">
+    <div class="mb-8 text-xs text-dark-blue sm:text-base">
         <p>Speed</p>
         <input class="w-full cursor-pointer accent-light-greenish-blue" type="range" min="1" max="1000"
             v-model="$store.state.speed" />
-        <div className="-mt-2 flex w-full justify-between">
-            <span className="text-gray-600">1</span>
-            <span className="text-gray-600">1000</span>
+        <div class="-mt-2 flex w-full justify-between">
+            <span class="text-gray-600">1</span>
+            <span class="text-gray-600">1000</span>
         </div>
     </div>
 </template>
